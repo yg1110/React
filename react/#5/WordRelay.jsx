192dc1a37c51db8bf1e179235c8822e390f679cb
yg1110 <younggil9488@gmail.com>
2020-06-20 14:53:52 +0900

    class -> hook

---
+++ react/#5/WordRelay.jsx
@@ -0,0 +1,48 @@
+const React = require("react");
+const { useState, useRef } = React;
+
+const WordRelay = () => {
+  const [word, setWord] = useState("끝말잇기");
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const inputRef = useRef(null);
+
+  const onSubmitForm = e => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setResult("딩동댕");
+      setWord(value);
+      setValue("");
+      inputRef.current.focus();
+    } else {
+      setResult("땡");
+      setValue("");
+      inputRef.current.focus();
+    }
+  };
+
+  const onChange = e => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor="wordInput">글자를 입력하세요.</label>
+        <input
+          id="wordInput"
+          className="wordInput"
+          ref={inputRef}
+          value={value}
+          onChange={onChange}
+        />
+        <button>클릭</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+};
+
+module.exports = WordRelay;
