151f98267d5fd254428fd42cb049bf51a0f36618
yg1110 <younggil9488@gmail.com>
2020-06-25 17:29:02 +0900

    조건문, setTimeout

--- react/#8/ResponseCheck-class.jsx
+++ react/#8/ResponseCheck-class.jsx
@@ -8,14 +8,51 @@ class ResponseCheck extends PureComponent {
   };
 
   onClickScreen = () =>{
-    
-  };
+    const { state, message, result} = this.state;
+    if(state === 'waiting'){
+      this.setState({
+        state: 'ready',
+        message: '조록색이 되면 클릭하세요.'
+      });
+      this.timeout = setTimeout( () => {
+        this.setState({
+          state:'now',
+          message: '지금 클릭'
+        })
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000)
+    } else if( state === 'ready'){
+      clearTimeout(this.timeout);
+      this.setState({
+        state: 'waiting',
+        message: '너무 빠름'
+      })
+    } else if( state === 'now'){
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          message: '클릭해서 시작하세요.',
+          result: [...prevState.result, this.endTime - this.startTime]
+        }
+      })
+    }
+  }
+
+  onReset = () =>{
+    this.setState({
+      result: []
+    })
+  }
 
   renderAverage = () =>{
     const { result } = this.state;
     return result.length ===0
     ? null
-    : <div>평균시간: {result.reduce((a,c) => a+c)/ this.state.result.length}ms</div>
+    : <>
+      <div>평균시간: {result.reduce((a,c) => a+c)/ this.state.result.length}ms</div>
+      <button onClick={this.onReset}>리셋</button>
+    </>
   }
 
   render() {
--- react/#8/client.jsx
+++ react/#8/client.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 import { hot } from "react-hot-loader/root";
 
-import ResponsCheck from "./ResponseCheck-class";
+// import ResponsCheck from "./ResponseCheck-class";
+import ResponsCheck from "./ResponseCheck-hooks";
 
 const Hot = hot(ResponsCheck);
 
